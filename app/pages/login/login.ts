34c8b73e6993ff28c9c7c4d1ad1faed9179d900b
Ccoolboy <michaelgira23@gmail.com>
2016-07-05 19:38:58 -0600

    Create login form

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,11 +1,24 @@
 import {Component} from '@angular/core';
-
+import {FormBuilder, Validators} from '@angular/common';
 
 @Component({
   templateUrl: 'build/pages/login/login.html'
 })
 export class LoginPage {
-  constructor() {
+  loginForm: any;
 
+  constructor(form: FormBuilder) {
+    // Create a form group
+    this.loginForm = form.group({
+      // All inputs are required
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  // Fires when form submitted
+  login(event) {
+    event.preventDefault();
+    console.log(this.loginForm.value);
   }
 }
